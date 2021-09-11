6985cf8608a41901d00351aa848274b5be4996e2
Felipe Lacerda Amorim <joaofelipeamorim2001@gmail.com>
2021-09-11 17:42:37 -0300

    Basic static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,90 @@
+const DataTable = () => {
+    return (
+        <div className="table-responsive">
+            <table className="table table-stripped table-sm">
+                <thead>
+                    <th>Data</th>
+                    <th>Vendedor</th>
+                    <th>Clientes visitados</th>
+                    <th>Negócios fechados</th>
+                    <th>Valor</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+  }
+  
+  export default DataTable;
+  
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/FelipeLacerdaAmorim" target="_blank" rel="noreferrer">Felipe Amorim</a></p>
+                <p className="text-light"><small><strong>Semana Spring</strong><br />
+                Evento promovido pela escola DevSuperior: <a href="https:instagram.com/devsuperior.ig" target="blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+  }
+  
+  export default Footer;
+  
