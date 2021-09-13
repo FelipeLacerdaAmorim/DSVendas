9213069eab9a8fc4ba3a0ab0a8fe3d5b31dbaef6
Felipe Lacerda Amorim <joaofelipeamorim2001@gmail.com>
2021-09-12 23:13:47 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -24,16 +24,12 @@ const DonutChart = () => {
         }));
     }, [])
 
-
     const options = {
         legend: {
             show:  true
         }
     };
-    /*const mockData = {
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé'],
-        series: [477138, 499928, 444867, 220426, 473088]
-    }*/
+
     return (
         <Chart 
             options={{ ...options, labels: chartData.labels}}
